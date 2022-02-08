8a295dd03467a2e1fcf06b1334e60728a2689c81
onulee <onulee@naver.com>
2022-02-08 14:18:22 +0900

    commit

--- dataClass/pt0208/bpjt/fboard/board/urls.py
+++ dataClass/pt0208/bpjt/fboard/board/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 
 app_name='board'
 urlpatterns = [
-    path('blist/',views.blist,name='blist' ),
-    path('bwrite/',views.bwrite,name='bwrite' ),
-    path('bwriteOk/',views.bwriteOk,name='bwriteOk' ),
-    path('<str:b_no>/bview/',views.bview,name='bview' ),
+    path('blist/',views.blist,name='blist'),
+    path('bwrite/',views.bwrite,name='bwrite'),
+    path('bwriteOk/',views.bwriteOk,name='bwriteOk'),
+    path('<str:b_no>/bview/',views.bview,name='bview'),
+    path('<str:b_no>/bmodify/',views.bmodify,name='bmodify'), 
+    path('bmodifyOk/',views.bmodifyOk,name='bmodifyOk' ),  # 수정저장
 ]
--- dataClass/pt0208/bpjt/fboard/board/views.py
+++ dataClass/pt0208/bpjt/fboard/board/views.py
@@ -1,3 +1,5 @@
+
+from datetime import timezone
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -23,7 +25,27 @@ def bwriteOk(request):
     
     return HttpResponseRedirect(reverse('board:blist'))
 
+# 게시판 뷰페이지
 def bview(request,b_no):
-    
-    context={}
+    qs = Freeboard.objects.get(b_no=b_no)
+    context={'board':qs}
     return render(request,'content_view.html',context)
+
+# 게시판 수정페이지
+def bmodify(request,b_no):
+    qs = Freeboard.objects.get(b_no=b_no)
+    context={'board':qs}
+    return render(request,'modify_view.html',context)
+
+# 게시판수정 저장
+def bmodifyOk(request):
+    b_no = request.POST.get('b_no')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    qs = Freeboard.objects.get(b_no=b_no)
+    qs.b_title = title
+    qs.b_content = content
+    # qs.b_date = timezone.now()    #2개 1개 최초등록, 수정업데이트
+    qs.save()
+    
+    return HttpResponseRedirect(reverse('board:blist'))
