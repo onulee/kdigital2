d04cb440d988aa7bad70defba7c90525547ae2e6
onulee <onulee@naver.com>
2022-02-08 12:17:42 +0900

    commit

--- dataClass/pt0208/bpjt/fboard/board/urls.py
+++ dataClass/pt0208/bpjt/fboard/board/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 app_name='board'
 urlpatterns = [
     path('blist/',views.blist,name='blist' ),
+    path('bwrite/',views.bwrite,name='bwrite' ),
+    path('bwriteOk/',views.bwriteOk,name='bwriteOk' ),
+    path('<str:b_no>/bview/',views.bview,name='bview' ),
 ]
--- dataClass/pt0208/bpjt/fboard/board/views.py
+++ dataClass/pt0208/bpjt/fboard/board/views.py
@@ -1,5 +1,29 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from board.models import Freeboard
 
-# Create your views here.
+# 게시판리스트
 def blist(request):
-    return render(request,'blist.html')
+    qs = Freeboard.objects.all().order_by('-b_no')
+    context = {'blist':qs }
+    return render(request,'blist.html',context)
+
+# 게시판 글쓰기
+def bwrite(request):
+    return render(request,'write_view.html')
+
+# 게시판글쓰기 저장
+def bwriteOk(request):
+    id = request.POST.get('id')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    qs = Freeboard(b_id=id,b_title=title,b_content=content)
+    qs.save()
+    
+    return HttpResponseRedirect(reverse('board:blist'))
+
+def bview(request,b_no):
+    
+    context={}
+    return render(request,'content_view.html',context)
