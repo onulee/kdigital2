401bccc637e8ef2336113bfa19a33aae1a264d99
onulee <onulee@naver.com>
2022-02-09 12:19:24 +0900

    search commit

--- dataClass/pt0209/community/comm/board/views.py
+++ dataClass/pt0209/community/comm/board/views.py
@@ -3,9 +3,25 @@ from board.models import Fboard
 
 # 게시판리스트
 def blist(request):
-    qs = Fboard.objects.all().order_by('-b_no')
-    context={'blist':qs}
-    return render(request,'blist.html',context)
+    if request.method == 'GET':
+        qs = Fboard.objects.all().order_by('-b_no')
+        context={'blist':qs}
+        return render(request,'blist.html',context) 
+    else:
+        category = request.POST.get('category')
+        searchword = request.POST.get('searchword')
+        # title검색
+        if searchword == 'title':
+            qs = Fboard.objects.filter(b_title__contains=searchword)
+            context={'blist':qs}
+            return render(request,'blist.html',context) 
+            
+        elif searchword == 'content':    
+            qs = Fboard.objects.filter(b_content__contains=searchword)
+            context={'blist':qs}
+            return render(request,'blist.html',context) 
+
+    
 
 # 뷰페이지
 def bview(request,b_no):
@@ -47,4 +63,10 @@ def bwriteOk(request):
     qs = Fboard(b_id=id,b_title=title,b_content=content)
     qs.save()
     # Fboard.objects.create(b_id=id,b_title=title,b_content=content)
+    return redirect('board:blist')
+
+# 게시글 삭제
+def bdelete(request,b_no):
+    qs = Fboard.objects.get(b_no=b_no)
+    qs.delete()
     return redirect('board:blist')
