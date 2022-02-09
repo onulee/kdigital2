98097cf496fbc3f946660cd736811a3966bd3886
onulee <onulee@naver.com>
2022-02-09 17:03:32 +0900

    commit

--- dataClass/pt0209/community/comm/member/urls.py
+++ dataClass/pt0209/community/comm/member/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 app_name='member'
 urlpatterns = [
     path('login/', views.login,name='login'),
-    # path('logout/', views.logout,name='logout'),
+    path('logout/', views.logout,name='logout'),
 ]
