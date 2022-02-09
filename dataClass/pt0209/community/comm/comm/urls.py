cf6362bfe21d74117db1e36aed531e682cb81985
onulee <onulee@naver.com>
2022-02-09 16:19:58 +0900

    commit

--- dataClass/pt0209/community/comm/comm/urls.py
+++ dataClass/pt0209/community/comm/comm/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('board/', include('board.urls')),
+    path('member/', include('member.urls')),
 ]
