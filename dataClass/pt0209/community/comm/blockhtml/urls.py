032958913340ca9e18169d1a5480ceb9f719b2e8
onulee <onulee@naver.com>
2022-02-10 13:58:47 +0900

    commit

--- dataClass/pt0209/community/comm/blockhtml/urls.py
+++ dataClass/pt0209/community/comm/blockhtml/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 app_name='blockhtml'
 urlpatterns = [
     path('notice_list/', views.notice_list,name='notice_list'),
+    path('notice_read/', views.notice_read,name='notice_read'),
     
 ]
--- dataClass/pt0209/community/comm/blockhtml/views.py
+++ dataClass/pt0209/community/comm/blockhtml/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def notice_list(request):
+    return render(request,'notice_list.html')
+
+def notice_read(request):
+    return render(request,'notice_read.html')
