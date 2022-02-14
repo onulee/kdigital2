f027f305aec8cc03e5d26f943392d134420ccef4
onulee <onulee@naver.com>
2022-02-14 18:19:25 +0900

    commit

--- dataClass/web0214/w0214_13.py
+++ dataClass/web0214/w0214_13.py
@@ -12,18 +12,25 @@ res.raise_for_status()
 
 soup = BeautifulSoup(res.text,"lxml")
 
+# div 중 id가 content > table
 tbody1 = soup.find("div",{"id":"content"}).table
+# tr 모두 찾음
 cartoons = tbody1.find_all("tr")
-count = 0
-total = 0
+count = 0    # 평점이 있는 개수
+total = 0    # 합계
 for cartoon in cartoons:
+    # td class=title만 출력
     if cartoon.find("td",{"class":"title"}):
        print(cartoon.find("td",{"class":"title"}).find("a").get_text())
+       # 개수 1증가
        count += 1
+    # div class=rating_type만 출력 
     if cartoon.find("div",{"class":"rating_type"}):
+       # 평점 합계
        total = total + float(cartoon.find("div",{"class":"rating_type"}).strong.get_text())
        print(cartoon.find("div",{"class":"rating_type"}).strong.get_text())
-       
+
+# 합계출력 소수점 2자리까지 출력       
 print("합계 : ",round(total,2))           
 print("평균 : ",round(total/count,2))           
     
