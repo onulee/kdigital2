1f42c47b494c12b3e322ac466237c702032f6d04
onulee <onulee@naver.com>
2022-02-15 13:55:05 +0900

    commit

--- dataClass/web0215/w0215_02.py
+++ dataClass/web0215/w0215_02.py
@@ -10,20 +10,42 @@ res = requests.get(url,headers)
 res.raise_for_status()
 soup = BeautifulSoup(res.text,"lxml")
 
+# ul > li 모든 상품의 전체 리스트를 가져오기
 items = soup.find("div",{"id":"poduct_list_area"}).find_all("li",{"class":"list_4 adcno3"})
 
-name = items[0].find("img",{"class":"lazy"})["alt"]
-price = items[0].find("div",{"class":"price"}).b.get_text()
-score = items[0].find("p",{"class":"score"}).em.get_text()
-review = items[0].find("p",{"class":"score"}).find("span").next_sibling.strip()
-print(name)
-print(price)
-print(score)
-print(review)
-
-# for item in items:
-#     name = item.find("div",{"class":"name"})
-#     print(name)
+# 리스트 넘어온 것을 반복해서 출력
+for idx,item in enumerate(items):
+    name = item.find("img",{"class":"lazy"})["alt"]
+    price = item.find("div",{"class":"price"}).b.get_text()
+    
+    # 평점 : 평점이 있는 것만 출력 없으면 넘어감.
+    if item.find("p",{"class":"score"}).em:
+        score = item.find("p",{"class":"score"}).em.get_text()
+        # 평점이 9.5이상만 출력
+        if float(score)>=9.5:
+            pass
+        else:
+            continue  # 평점 9.5이하는 출력하지 않음.
+    else:
+        continue  
+    
+    # 리뷰 : span을 찾아서 다음에 있는 글자를 출력, strip은 빈공백 제거함수
+    if item.find("p",{"class":"score"}).find("span").next_sibling.strip():
+        review = item.find("p",{"class":"score"}).find("span").next_sibling.strip()
+        # 평점에 있는 괄호제거 (721) -> 721 숫자만 남김.
+        review = review[1:-1]
+        if int(review)>=100:
+            pass
+        else:
+            continue  # 100개이하는 출력하지 않음.
+    else:
+        continue 
+    print("[ {} ]".format(idx+1))   
+    print("제목 : ",name)
+    print("가격 : ",price)
+    print("평점 : ",score)
+    print("리뷰 : ",review)
+    print("-"*20)
     
 
-# print(len(items))
+print(len(items))
