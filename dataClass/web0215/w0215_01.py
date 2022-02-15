844a99d3589feeb02df2d4c9e1d7b952a5fee62d
onulee <onulee@naver.com>
2022-02-15 11:16:15 +0900

    commit

--- dataClass/web0215/w0215_01.py
+++ dataClass/web0215/w0215_01.py
@@ -2,25 +2,55 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url='https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=6&backgroundColor='
-headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
-res = requests.get(url,headers=headers)
-res.raise_for_status()
-soup = BeautifulSoup(res.text,'lxml')
+for page in range(1,6):
+    url='https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page={}&rocketAll=false&searchIndexingToken=1=6&backgroundColor='.format(page)
+    headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"}
+    res = requests.get(url,headers=headers)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text,'lxml')
 
-# ul1 = soup.find("ul",{"id":"productList"})
-items = soup.find_all("li",{"class":re.compile("^search-product")})
+    # ul1 = soup.find("ul",{"id":"productList"})
+    items = soup.find_all("li",{"class":re.compile("^search-product")})
 
-for item in items:
-    name = item.find("div",{"class":"name"}).get_text()
-    price = item.find("strong",{"class":"price-value"}).get_text()
-    print(name)
-    print(price)
-    # 평점이 없는 경우 발생
-    rating = item.find("em",{"class":"rating"})
-    if rating:
-        print(rating.get_text())
-    else:
-        print("[ 평점 없음 ]")    
+    for item in items:
+        # 상품명, 가격
+        name = item.find("div",{"class":"name"}).get_text()
+        price = item.find("strong",{"class":"price-value"}).get_text()
+        pro_url = item.find("a")['href']
+        
+        # 평점이 없는 경우 발생
+        rating = item.find("em",{"class":"rating"})
+        # <em>5.0</em></em>
+        if rating:
+            # rating글자 추출
+            rate = rating.get_text()
+            
+        else:
+            continue    
+            # print("[ 평점 없음 ]")
 
-# print(len(items))
+        # 상품평
+        totalcount = item.find("span",{"class":"rating-total-count"})
+        if totalcount:
+            tcnt = totalcount.get_text()
+            # print(tcnt)
+            tcnt = tcnt[1:-1]  # (20) -> 20 
+            # print(tcnt)
+            if int(tcnt)>=1000 and float(rate) >=5.0:
+                print("상품명 : ",name)
+                print("상품가격 : ",price)  
+                print("평점 : ",rating.get_text())
+                print("상품평 : ",tcnt)
+                print("https://www.coupang.com"+pro_url)
+                print("-"*10)
+            else:
+                continue    
+        else:
+            continue
+            # print("[ 상품평 없음 ]") 
+    
+      
+        
+        
+    
+         
